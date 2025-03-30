73835bc82808d0b72dd16a879425b293e2d3250a
KienCT <tamkiencao@gmail.com>
2025-03-31 02:14:30 +0700

    feat: Highlight king in check and improve check detection
    This commit introduces highlighting for the king when it is in check,
    improving the user's awareness of this critical game state.
    
    The check detection logic has been refactored for clarity and
    correctness. Instead of relying on a complex conditional check,
    the code now iterates through all opponent pieces to identify checks.
    
    Additionally, code minification has been enabled in the Gradle
    configuration to reduce the app's size.

---
+++ app/src/main/java/com/kienct/chess/ui/chess/square/decoration/DefaultCheckedSquare.kt
@@ -0,0 +1,14 @@
+package com.kienct.chess.ui.chess.square.decoration
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * A default [CheckedSquare] object with a red color and an alpha value of 0.15f.
+ *
+ * This provides a readily available instance of a checked square with predefined styling.
+ * It's used to visually highlight the checked state of a king piece in chess.
+ */
+object DefaultCheckedSquare : CheckedSquare(
+    color = Color.Red,
+    alpha = 0.15f
+)
